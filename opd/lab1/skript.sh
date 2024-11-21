228f83988af01fcbd5c060818af1bfb054da322d
ni-hvosta-in <alpinefuchs@gmail.com>
2024-11-21 20:26:06 +0300

    Create skript.sh

---
+++ opd/lab1/skript.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/bash
+
+mkdir lab0
+cd lab0
+
+mkdir corphish4
+
+cd corphish4
+
+	echo "satk=6 sdef=5 spd=6" > luxio
+
+	mkdir yamask
+
+	echo "Ходы Confuse Ray ╪ Dark Pulse" > chandelure
+	echo "Hex╪ Flame Burst╪ Heat Wave Pain Split Sleep Talk Smog╪ Snore Spite" >> chandelure
+	echo "Trick" >> chandelure
+
+	echo "Тип диеты Herbivore" > hippopotas
+
+	echo "Тип диеты Herbivore" > exeggcute
+
+cd ..
+
+echo "Тип диеты Carnivore" > luxray6
+
+echo "Тип покемона" > meditite6
+echo "FIGHTING PSYCHIC" >> meditite6
+
+mkdir shelmet2 
+cd shelmet2
+
+	mkdir krookodile
+
+	echo "weigth=69.4 height=35.0 atk=13" > cranidos
+	echo "def=4" >> cranidos
+
+	mkdir darmanitan
+
+	echo "Тип покемона ROCK STEEL" > bastiodon
+
+	echo "satk=9 sdef=8" > gastrodon
+	echo "spd=4" >> gastrodon
+
+	mkdir tympole
+cd ..
+
+mkdir starmie0
+
+	cd starmie0
+
+	mkdir croconaw
+
+	mkdir trubbish
+
+	echo "Развитые способности Sand Force" > shellos 
+
+	echo "Способности" > magmar
+	echo "Blaze Flame Body Flash Fire" >> magmar
+
+cd ..
+
+echo "weigth=76.1 height=31.0 atk=5" > staryu7
+echo "def=6" >> staryu7
+
+cd lab0
+
+chmod 046 luxray6
+chmod 400 meditite6
+
+chmod ug=r,o=--- corphish4/luxio
+#chmod 440 corphish4/luxio
+chmod 046 corphish4/chandelure
+chmod 640 corphish4/exeggcute
+chmod u-rwx,g-wx,o=rw  corphish4/hippopotas
+chmod u=wx,g=rw,o=x corphish4/yamask
+chmod 764 corphish4
+
+chmod 404 shelmet2/cranidos
+chmod 066 shelmet2/bastiodon
+chmod 400 shelmet2/gastrodon
+chmod 335 shelmet2/darmanitan
+chmod 312 shelmet2/krookodile
+chmod 575 shelmet2/tympole
+chmod 753 shelmet2
+
+chmod 004 starmie0/shellos
+chmod 046 starmie0/magmar
+chmod 711 starmie0/croconaw
+chmod 750 starmie0/trubbish
+chmod 317 starmie0
+
+chmod 046 staryu7
+
+
+
+chmod u+r corphish4/chandelure corphish4/yamask corphish4/hippopotas
+cp -R corphish4 starmie0/croconaw
+chmod u-r corphish4/chandelure corphish4/yamask corphish4/hippopotas
+
+chmod u+r starmie0
+cd starmie0/
+ls -p1R ./croconaw
+cd ..
+chmod u-r starmie0
+
+chmod u+r shelmet2/bastiodon
+chmod u+r corphish4/hippopotas
+cat corphish4/hippopotas shelmet2/bastiodon > luxray6_36
+cat corphish4/hippopotas shelmet2/bastiodon luxray6_36
+chmod u-r shelmet2/bastiodon
+chmod u-r corphish4/hippopotas
+
+cat meditite6 > corphish4/luxiomeditite 
+cat meditite6 corphish4/luxiomeditite
+
+ln -s stramie0 Copy_66
+ls -l Copy_66
+
+ln -s ../staryu7 shelmet2/cranidosstaryu
+cat shelmet2/cranidosstaryu
+				
+chmod u+r luxray6
+cp luxray6 starmie0/trubbish
+ls starmie0/trubbish/luxray6
+chmod u-r luxray6
+
+chmod u+r luxray6
+ln luxray6 shelmet2/bastiodonluxray
+ls -ip1 luxray6 shelmet2/bastiodonluxray
+chmod u-r luxray6
+
+
+wc -m l* */l* */*/l* 2>/dev/null| grep -v " total" | sort
+ls -laRt |grep '^[-l]'|grep '[012][0-9]:[0-6][0-9] s'
+ls -laR |grep '^[-l]'|grep '[012][0-9]:[0-6][0-9] s' |sort -rk8
+ls -Rla 2>/tmp/error.err | grep '^[-l]' | grep -E '(6 -> |6$)' | sort -k9|head -3 
+cat meditite6 | wc -m >> meditite6 2>/tmp/error.err
+cd starmie0
+ls -p1 | grep -v '^./'| grep -v '/$' | grep -i '^[a-z]' | sort	
