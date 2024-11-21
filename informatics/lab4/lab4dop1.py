57e7342ae07c72628e100aeccbcfafa21ac9bca2
ni-hvosta-in <alpinefuchs@gmail.com>
2024-11-21 22:51:27 +0300

    Update lab4dop1.py

--- informatics/lab4/lab4dop1.py
+++ informatics/lab4/lab4dop1.py
@@ -2,6 +2,6 @@ import json
 import xmltodict
 fr_xml = open("lab4input.xml")
 dict = xmltodict.parse(fr_xml.read())
-json_data = json.dumps(dict)
-fw_json = open("lab4.json","w")
+json_data = json.dumps(dict, ensure_ascii=False, indent=4)
+fw_json = open("lab4dop1.json","w")
 fw_json.write(json_data)
